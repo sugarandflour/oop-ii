86021f0670dcb0009c20dc6c7431a74424c08a8b
sugarandflour <benson.ndolo@strathmore.edu>
2025-05-02 09:26:21 +0300

    add player

---
+++ oop/Player.java
@@ -0,0 +1,10 @@
+public class Player{
+
+    void play(){
+        System.out.println("Ready...Go!!");
+    }
+
+    public static void main(String[] args){
+        (new Player()).play();
+    }
+}
